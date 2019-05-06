973c407b7bdd868efb1f46a8de08cbea0ad1990d
Oscar7z <loco-id@hotmail.com>
2019-05-05 20:20:47 -0500

    Commit Inicial

---
+++ Assets/Scripts/BirdBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdBehaviour : MonoBehaviour
+
+
+{
+    private Rigidbody2D rbBird;
+    private float fUp = 250f;
+    private bool dead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbBird = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead) return;
+
+        if (Input.GetKeyDown("space")) 
+        {
+            rbBird.velocity = Vector2.zero;
+            rbBird.AddForce(Vector2.up * fUp);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        dead = true;
+        Debug.Log("Dead");
+    }
+}
---
+++ Assets/Scripts/Obstaculos.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculos : MonoBehaviour
+{
+
+    public float v = 5f;
+    public float mH = 24.33f;
+    public float mV; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        mV = Random.Range(3.80f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var p = gameObject.transform.position;
+        mH -= v;
+        p.x = mH;
+        p.y = mV;
+
+        gameObject.transform.position = p;
+
+        if(mH < -24f) 
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
